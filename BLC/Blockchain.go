dbb3c5c10152ea34a993aff6aa5aed9f1467598b
zhao <zhoubk1451@163.com>
2021-05-14 19:30:45 +0800

    区块序列化

--- BLC/Blockchain.go
+++ BLC/Blockchain.go
@@ -1,20 +1,119 @@
 package BLC
 
+import (
+	"fmt"
+	"github.com/boltdb/bolt"
+	"log"
+	"math/big"
+	"time"
+)
+
+const dbName = "blockchain.db"
+const blockTableName = "blockchain"
+
 type Blockchain struct {
-	Blocks []*Block //储存区块
+	//Blocks []*Block //储存区块，之后需要改成持久化存贮
+	Tip []byte   //最新区块的hash值
+	DB  *bolt.DB //数据库
 }
 
 //创建带有创世区块的区块链
 func CreateBlockchainWithGenesisBlock() *Blockchain {
-
-	genesisBlock := CreateGenesisBlock("Genesis data")
-	return &Blockchain{[]*Block{genesisBlock}}
+	db, err := bolt.Open(dbName, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var blockHash []byte
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blockTableName))
+		//检查表是否存在
+		if b == nil {
+			//创建表
+			b, err = tx.CreateBucket([]byte(blockTableName))
+			if err != nil {
+				log.Panic(err)
+			}
+		}
+		if b != nil {
+			genesisBlock := CreateGenesisBlock("Genesis data")
+			err := b.Put(genesisBlock.Hash, genesisBlock.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+			//	存储最新的区块的hash
+			err = b.Put([]byte("l"), genesisBlock.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+			blockHash = genesisBlock.Hash
+		}
+		return nil
+	})
+	return &Blockchain{blockHash, db}
 }
 
 //增加区块到链
-func (blc *Blockchain) AddBlockToBlockchain(data string, height int64, preHash []byte) {
-	//创建新区块
-	newBlock := NewBlock(data, height, preHash)
-	//增加到blc上
-	blc.Blocks = append(blc.Blocks, newBlock)
+func (blc *Blockchain) AddBlockToBlockchain(data string) {
+	err := blc.DB.Update(func(tx *bolt.Tx) error {
+		//获取表
+		b := tx.Bucket([]byte(blockTableName))
+		if b != nil {
+			//获取数据库最新区块
+			blockBytes := b.Get(blc.Tip)
+			block := DeserializeBlock(blockBytes)
+			//创建区块
+			newBlock := NewBlock(data, block.Height+1, block.Hash)
+			//保存到数据库
+			err := b.Put(newBlock.Hash, newBlock.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+			//更新tip 数据库+内存
+			err = b.Put([]byte("l"), newBlock.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+			blc.Tip = newBlock.Hash
+		}
+
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
+//遍历打印所有区块信息
+func (blc *Blockchain) PrintChain() {
+	var block *Block
+	var currentHash []byte = blc.Tip
+	for {
+		err := blc.DB.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(blockTableName))
+			if b != nil {
+				//获取当前区块
+				blockBytes := b.Get(currentHash)
+				block = DeserializeBlock(blockBytes)
+				fmt.Println("===================")
+				fmt.Printf("height : %d\n", block.Height)
+				fmt.Printf("PrevBlockHash : %x\n", block.PrevBlockHash)
+				fmt.Printf("Data : %s\n", block.Data)
+				fmt.Printf("Timestamp : %s\n", time.Unix(block.Timestamp, 0).Format("2006-01-02 03:04:05 PM"))
+				fmt.Printf("Hash : %x\n", block.Hash)
+				fmt.Printf("Nonce : %d\n", block.Nonce)
+				fmt.Println("===================")
+			}
+			return nil
+		})
+		if err != nil {
+			log.Panic(err)
+		}
+		var hashInt big.Int
+		hashInt.SetBytes(block.PrevBlockHash)
+		if big.NewInt(0).Cmp(&hashInt) == 0 {
+			break
+		}
+		currentHash = block.PrevBlockHash
+
+	}
 }
--- main.go
+++ main.go
@@ -1,21 +1,18 @@
 package main
 
-import "fmt"
-import "helloworldblock/BLC"
+import (
+	"helloworldblock/BLC"
+)
 
 func main() {
 
 	Blockchain := BLC.CreateBlockchainWithGenesisBlock()
-	//创世区块
-	fmt.Println(Blockchain.Blocks)
-	//	新区快
-	Blockchain.AddBlockToBlockchain("send 100RMB to zhangsan", Blockchain.Blocks[len(Blockchain.Blocks)-1].Height+1, Blockchain.Blocks[len(Blockchain.Blocks)-1].Hash)
-	Blockchain.AddBlockToBlockchain("send 300RMB to zhangsan", Blockchain.Blocks[len(Blockchain.Blocks)-1].Height+1, Blockchain.Blocks[len(Blockchain.Blocks)-1].Hash)
-	Blockchain.AddBlockToBlockchain("send 500RMB to zhangsan", Blockchain.Blocks[len(Blockchain.Blocks)-1].Height+1, Blockchain.Blocks[len(Blockchain.Blocks)-1].Hash)
-	fmt.Println(Blockchain.Blocks)
-	bytes := Blockchain.Blocks[1].Serialize()
-	fmt.Println(bytes)
-	block := BLC.DeserializeBlock(bytes)
-	fmt.Print(block)
+	defer Blockchain.DB.Close()
+
+	////	新区快
+	Blockchain.AddBlockToBlockchain("send 100RMB to zhangsan")
+	Blockchain.AddBlockToBlockchain("send 300RMB to zhangsan")
+	Blockchain.AddBlockToBlockchain("send 500RMB to zhangsan")
+	Blockchain.PrintChain()
 
 }
