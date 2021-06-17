5f26d1a84aa8f94545f47ee5165cab5c95b93e3b
ZbkSou <zhoubk1451@163.com>
2021-06-17 23:51:59 +0800

    ..

---
+++ BLC/CLI.go
@@ -0,0 +1,59 @@
+package BLC
+
+import (
+	"fmt"
+	"os"
+	"flag"
+	"log"
+)
+type CLI struct {
+	Blockchain *Blockchain
+}
+func printUsage(){
+	fmt.Println("Usage:")
+	fmt.Println("\t addblock -data DATA -- 交易数据")
+	fmt.Println("\t printchain -- 输出区块信息")
+}
+
+func isValidArgs()  {
+	if len(os.Args)<2{
+		printUsage()
+		os.Exit(1)
+	}
+
+}
+
+
+func (cli *CLI)Run() {
+	isValidArgs()
+	addBlockCmd := flag.NewFlagSet("addblock", flag.ExitOnError)
+	printChainCmd := flag.NewFlagSet("printchain", flag.ExitOnError)
+
+	flagAddBlockData := addBlockCmd.String("data","zbk","交易数据")
+
+	switch os.Args[1] {
+	case "addblock":
+		err:= addBlockCmd.Parse(os.Args[2:])
+		if err!=nil{
+			log.Panic(err)
+		}
+	case "printchain":
+		err:= addBlockCmd.Parse(os.Args[2:])
+		if err!=nil{
+			log.Panic(err)
+		}
+	default:
+		printUsage()
+		os.Exit(1)
+	}
+	if addBlockCmd.Parsed(){
+		if *flagAddBlockData ==""{
+			printUsage()
+			os.Exit(1)
+		}
+	}
+	if printChainCmd.Parsed(){
+		fmt.Println("print all data")
+	}
+
+}
