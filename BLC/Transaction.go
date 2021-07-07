e38413478cf8aae506258191878d65010e7cff69
ZbkSou <zhoubk1451@163.com>
2021-07-07 23:58:16 +0800

    指令操作

--- BLC/Transaction.go
+++ BLC/Transaction.go
@@ -1,5 +1,6 @@
 package BLC
 
+//UTXO
 type Transaction struct {
 	//1 交易hash
 	TxHash []byte
@@ -8,3 +9,19 @@ type Transaction struct {
 	//3 输出
 	Vouts []*TXOutput
 }
+
+//1 Transaction 创建分两种情况
+//1.1 区块链创建时transaction
+
+func NewCoinBaseTransaction(address string) *Transaction {
+
+	txInput := &TXintput{[]byte{}, -1, "Genesis Data"}
+	txOutput := &TXOutput{10, address}
+	txCoinBase := &Transaction{
+		[]byte{},
+	}
+	return &Transaction{}
+
+}
+
+//1.2 转账产生transaction
